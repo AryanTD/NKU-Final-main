ac2d7d5d469698e9ec2a9a96109d24cc1d1ec8df
AryanTD <aryantandon285@gmail.com>
2022-05-13 13:58:10 -0400

    Initial Commit

---
+++ Characters/CharacterList.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+import Kingfisher
+
+
+struct CharacterList: View {
+    
+    @State var characters = [MarvelCharacter]()
+ 
+    
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                List{
+                    ForEach(characters, id: \.self){ char in
+                        CharacterRow(character: char)
+                            .onAppear()
+                    }
+                }
+               
+            }
+            .navigationTitle("Characters")
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
+    
+    func getCharacters() {
+        let networkManager = NetworkManager()
+        
+        networkManager.getCharacters(offset: 0, limit: 100) { container, error in
+            
+            guard let container = container, let characters = container.results, error == nil else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.characters = characters
+            }
+        }
+    }
+}
+
+struct CharacterList_Previews: PreviewProvider {
+    static var previews: some View {
+        CharacterList(characters: MarvelCharacter.characters)
+    }
+}
